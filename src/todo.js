4a1cac9d3bff7e4e63031d001709b1eb19a1dfb9
Vishal Sood <vishal.chandra.sood@gmail.com>
2017-05-09 16:31:08 +0200

    interact with enter as well

--- src/todo.js
+++ src/todo.js
@@ -1,14 +1,31 @@
 var main = function() {
 	"use strict";
-	$(".todo-input button").on("click", function (event) {
-		console.log("Hello World! Whats TODO today?")
+
+  var addTodoFromInputBox = function() {
+    var $new_todo;
     var $input_todo = $(".todo-input input");
     if ($input_todo.val() !== "") {
-		  var $new_todo = $("<li>").text($input_todo.val());
+		  $new_todo = $("<li>").text($input_todo.val());
+      $new_todo.hide();
 		  $("section.todos ol").append($new_todo);
+      $new_todo.fadeIn();
     }
 
+    $input_todo.val("");
+  };
+
+	$(".todo-input button").on("click", function (event) {
+		console.log("Hello World! Whats TODO today?")
+    addTodoFromInputBox();
 	});
+
+	$(".todo-input input").on("keypress", function (event) {
+    var $new_todo;
+    if (event.keyCode == 13) {
+      addTodoFromInputBox();
+    }
+	});
+
 };
 
 $(document).ready(main);
