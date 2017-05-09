17adbbfcd8423d36617a061ff6d73ed97cb28b6b
Vishal Sood <vishal.chandra.sood@gmail.com>
2017-05-09 16:19:46 +0200

    interactively add todos

--- src/todo.js
+++ src/todo.js
@@ -1,6 +1,14 @@
 var main = function() {
 	"use strict";
-	window.alert("hello world, lets decide whats TODO?");
+	$(".todo-input button").on("click", function (event) {
+		console.log("Hello World! Whats TODO today?")
+    var $input_todo = $(".todo-input input");
+    if ($input_todo.val() !== "") {
+		  var $new_todo = $("<li>").text($input_todo.val());
+		  $("section.todos ol").append($new_todo);
+    }
+
+	});
 };
 
 $(document).ready(main);
