0d4024653f7777df1e674d26f2abfcae38770ebf
Vishal Sood <vishal.chandra.sood@gmail.com>
2019-05-14 21:47:38 +0200

    Use Joda time
    ...

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,11 +5,12 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.ws._
 import play.api.Play.current
-import java.util.Date
+//import java.util.Date
 import java.text.SimpleDateFormat
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-
+import org.joda.time.{DateTimeZone, DateTime}
+import org.joda.time.format.DateTimeFormat
 import model.SunInfo
 
 /**
@@ -30,25 +31,34 @@ class Application @Inject()(
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action.async { implicit request: Request[AnyContent] =>
-    /*
-    val date = new Date()
-    val dateStr = new SimpleDateFormat().format(date)
-    Future.successful { Ok(views.html.index(dateStr)) }
+  def index() =
+    Action.async { implicit request: Request[AnyContent] =>
+      /*
+       val date = new Date()
+       val dateStr = new SimpleDateFormat().format(date)
+       Future.successful { Ok(views.html.index(dateStr)) }
      */
 
-    val responseF =
-      ws.url(
-        "http://api.sunrise-sunset.org/json?lat=46.5517&lng=6.5586&formatted=0"
-      ).get()
-    responseF.map { response =>
-      Ok(views.html.index(
-           SunInfo(
-             (response.json \ "results" \ "sunrise").as[String],
-             (response.json \ "results" \ "sunset").as[String]
-           )
-         )
-      )
+      val responseF =
+        ws.url(
+          "http://api.sunrise-sunset.org/json?lat=46.5517&lng=6.5586&formatted=0"
+        ).get()
+      responseF.map { response =>
+        val formatter =
+          DateTimeFormat
+            .forPattern(
+              "HH:mm:ss")
+          .withZone(
+            DateTimeZone.forID(
+              "Europe/Zurich"))
+        Ok(views.html.index(
+             SunInfo(
+               formatter.print(
+                 DateTime.parse(
+                   (response.json \ "results" \ "sunrise").as[String])),
+               formatter.print(
+                 DateTime.parse(
+                   (response.json \ "results" \ "sunset").as[String])))))
+      }
     }
-  }
 }
